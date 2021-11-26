e7cb68447ba32e5f63a49c988a24a164d7a5ae68
5war00p <57614947+5war00p@users.noreply.github.com>
2021-11-26 20:12:13 +0530

    Update vendingMachine.ts
    added services

--- src/vendingMachine.ts
+++ src/vendingMachine.ts
@@ -98,6 +98,9 @@ const vendingMachine = createMachine<VMContext, VMEvents>(
     },
   },
   {
+    services: {
+      paymentFSM: (context) => (send) => {},
+    },
     actions: {
       setup: (context, event) => {
         context.required_amount = 0;
